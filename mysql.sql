9cf184d6d91d4af5e28b57c797a8cebae80f73ce
Nate <nathanw88@gmail.com>
2018-12-16 13:01:57 -0600

    added schema and seed

--- mysql.sql
+++ mysql.sql
@@ -1,14 +1,3 @@
--- create Mysql bamazon
--- create table products
- -- /////////////// table colums
--- id primary key not null
--- product_name "" not null
--- department_name "" not null
--- price(cost to customer) double not null
--- stock quantity int
-
--- add 10 mock product rows
-
 DROP DATABASE IF EXISTS bamazon_db;
 CREATE DATABASE bamazon_db;
 
@@ -16,24 +5,24 @@ use bamazon_db;
 
 CREATE TABLE products(
 	id INT(11) AUTO_INCREMENT NOT NULL,
-    product_name VARCHAR(11) NOT NULL,
-    deparment_name VARCHAR(11) NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
+    department_name VARCHAR(50) NOT NULL,
     price DECIMAL(11, 2) NOT NULL,
+    stock INT(11) DEFAULT 0,
     PRIMARY KEY(id)
     );
     
-    SELECT * FROM products
+SELECT * FROM products;
     
-       
-INSERT INTO products (product_name, department_name, price)
+INSERT INTO products (product_name, department_name, price, stock)
   values
-    ("item_1", "department_1", 1.99),
-    ("item_2", "department_2", 2.99),
-    ("item_3", "department_3", 3.99),
-    ("item_4", "deparment_4", 4.99),
-    ("item_5", "deparment_5", 5.99),
-    ("item_6", "deparment_6", 6.99),
-    ("item_7", "deparment_7", 7.99),
-    ("item_8", "deparment_8", 8.99),
-    ("item_9", "deparment_9", 9.99),
-    ("item_10", "deparment_10", 10.99);
+    ("Mixed nuts", "Snacks", 21.99, 10),
+    ("Toaster", "Kitchen", 12.99, 11),
+    ("Football", "Sports", 10.99, 30),
+    ("Running shoes", "Shoes", 69.99, 40),
+    ("Tent", "Hiking", 89.99, 15),
+    ("Backpack", "Hiking", 84.99, 7),
+    ("Laptop", "Electronics", 1099.99, 17),
+    ("Shirt", "Clothing", 10.99, 0),
+    ("Snowboard", "Sports", 186.99, 11),
+    ("Blender", "Kitchen", 118.99, 17);
